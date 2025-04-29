b73ffbe9cf791dd40466a9d4e1d8e318668bdd7a
shAn-kor <kjn0406@naver.com>
2025-04-30 00:18:28 +0900

    Feat: Add Distributed Lock Redisson 라이브러리 이용한 분산락 작성

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
+	// Redis
+	implementation ("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation ("org.redisson:redisson-spring-boot-starter:3.27.0") // ⭐ Redisson
+
     // DB
 	runtimeOnly("com.mysql:mysql-connector-j")
 
---
+++ src/main/java/kr/hhplus/be/server/infrastructure/lock/DistributedLockAop.java
@@ -0,0 +1,57 @@
+package kr.hhplus.be.server.infrastructure.lock;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class DistributedLockAop {
+    private final static String LOCK_KEY_PREFIX = "lock:";
+
+    private final RedissonClient redissonClient;
+    private final AopForTransaction aopForTransaction;
+
+    @Around("@annotation(kr.hhplus.be.server.infrastructure.lock.DistributedLock)")
+    public Object lock(ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        DistributedLock distributedLock = method.getAnnotation(DistributedLock.class);
+
+        String key = LOCK_KEY_PREFIX
+                + CustomSpringELParser.getDynamicValue(
+                        signature.getParameterNames()
+                , joinPoint.getArgs()
+                , distributedLock.key()
+        );
+        RLock rLock = redissonClient.getLock(key);
+
+        try{
+            boolean available = rLock.tryLock(distributedLock.waitTime(), distributedLock.leaseTime(), distributedLock.timeUnit());
+
+            if (!available) {
+                return false;
+            }
+
+            return aopForTransaction.proceed(joinPoint);
+        } catch (InterruptedException e) {
+            throw new InterruptedException(e.getMessage());
+        } finally {
+            try {
+                rLock.unlock();
+            } catch (IllegalMonitorStateException e) {
+                log.info("Redisson Lock Already Unlock" + e.getMessage());
+            }
+        }
+    }
+}
