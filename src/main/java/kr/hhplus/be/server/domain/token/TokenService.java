a2c30ebfe8f7418d9fab5ded610c8aa13bda2bc1
shAn-kor <kjn0406@naver.com>
2025-05-16 01:16:38 +0900

    Feat: Add Redis Waiting Queue 레디스 기반 대기열

--- src/main/java/kr/hhplus/be/server/domain/token/TokenService.java
+++ src/main/java/kr/hhplus/be/server/domain/token/TokenService.java
@@ -1,17 +1,40 @@
 package kr.hhplus.be.server.domain.token;
 
+import kr.hhplus.be.server.infrastructure.repository.TokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
 import java.nio.file.AccessDeniedException;
 
-public interface TokenService {
-    Token generateToken(TokenCommand command);
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+    private final TokenRepository tokenRepository;
 
-    Token getToken(TokenCommand tokenCommand);
+    public Token generateToken(TokenCommand command) {
+        return tokenRepository.generateToken(command.userId(), command.concertId());
+    }
 
-    void isValid(TokenCommand command) throws AccessDeniedException;
+    public Token getToken(TokenCommand tokenCommand) {
+        return tokenRepository.getToken(tokenCommand.userId(), tokenCommand.concertId());
+    }
 
-    void endToken(TokenCommand tokenCommand);
+    public void isValid(TokenCommand command) throws AccessDeniedException {
+        Token token = tokenRepository.getToken(command.userId());
+        if (token == null) {
+            throw new AccessDeniedException("대기열을 통과하지 못했습니다.");
+        }
+    }
 
-    void fillActiveQueue(TokenCommand command);
+    public void endToken(TokenCommand tokenCommand) {
+        tokenRepository.endToken(tokenCommand.userId(), tokenCommand.concertId());
+    }
 
-    void endActiveToken(TokenCommand command);
+    public void fillActiveQueue(TokenCommand command) {
+        tokenRepository.fillActiveQueue(command.userId(), command.concertId());
+    }
+
+    public void endActiveToken(TokenCommand command) {
+        tokenRepository.endActiveToken(command.userId(), command.concertId());
+    }
 }
