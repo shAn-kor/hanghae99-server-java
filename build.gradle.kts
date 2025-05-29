38bdc061140512a2a26757a7c9d7c970c3f3d23f
shAn-kor <kjn0406@naver.com>
2025-05-29 18:22:14 +0900

    Chore: Kafka Setting 카프카 초기 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,11 @@ dependencies {
 	testCompileOnly("org.projectlombok:lombok:1.18.30")
 	testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
 
-    // Test
+	// Kafka
+	implementation ("org.springframework.kafka:spring-kafka")
+
+
+	// Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.testcontainers:junit-jupiter")
