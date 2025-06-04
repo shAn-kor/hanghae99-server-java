4084e766b05e5a7a0b3c4d75bdf57fc10e98c8c1
shAn-kor <kjn0406@naver.com>
2025-06-05 00:01:33 +0900

    Refactor: Move Consumer Transactional To Service 트랜잭셔널은 서비스에서 동작하도록 수정

--- src/main/java/kr/hhplus/be/server/presentation/token/QueueEntryConsumer.java
+++ src/main/java/kr/hhplus/be/server/presentation/token/QueueEntryConsumer.java
@@ -9,7 +9,6 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -22,14 +21,15 @@ public class QueueEntryConsumer {
             groupId = "queue-entry-consumer-group",
             containerFactory = "kafkaListenerContainerFactory"
     )
-    @Transactional
     public void listen(ConsumerRecord<String, QueueEntryPayload> record, Acknowledgment ack) {
         QueueEntryPayload payload = record.value();
         log.info("▶️ Kafka 수신: {}", payload);
 
-        // Redis 대기열에 추가
-        tokenService.generateToken(TokenCommand.builder().userId(payload.getUserId()).concertId(payload.getConcertId()).build());
-
-        ack.acknowledge(); // 수동 커밋
+        try {
+            tokenService.generateToken(TokenCommand.builder().userId(payload.getUserId()).concertId(payload.getConcertId()).build()); // 비즈니스 로직 위임
+            ack.acknowledge(); // 성공했을 때만 수동 커밋
+        } catch (Exception e) {
+            log.error("❌ Kafka 처리 실패: {}", e.getMessage(), e);
+        }
     }
 }
