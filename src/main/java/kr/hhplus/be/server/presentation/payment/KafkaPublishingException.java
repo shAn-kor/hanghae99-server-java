1db65c66211add82616232f29de7434f4a1333cb
shAn-kor <kjn0406@naver.com>
2025-06-04 23:50:22 +0900

    Refactor: Add Payment Event Fail Exception 결제 이벤트 실패 시 예외 던지도록 수정

---
+++ src/main/java/kr/hhplus/be/server/presentation/payment/KafkaPublishingException.java
@@ -0,0 +1,6 @@
+package kr.hhplus.be.server.presentation.payment;
+
+public class KafkaPublishingException extends Throwable {
+    public KafkaPublishingException(String s, Exception e) {
+    }
+}
